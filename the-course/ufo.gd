014067cafc87f79534670c1b146b5dcfd6f07e50
C24462104 Jack Whelan <C24462104@myTUDublin.ie>
2025-02-27 12:08:16 +0000

    tweens

--- the-course/ufo.gd
+++ the-course/ufo.gd
@@ -19,12 +19,18 @@ extends CharacterBody2D
 
 var can_fire = false
 
+
+
 func _ready() -> void:
 	# Tween my scale using elastic	
 	if ! Engine.is_editor_hint():
 		scale = Vector2.ZERO
-		var tween = create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)
+		var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 		tween.tween_property(self, "scale", Vector2.ONE, 1)
+		tween.tween_property(self, "position", position + Vector2.RIGHT * 200, 2)
+		tween.tween_property(self, "position", position + Vector2.LEFT * 200, 2)
+		tween.tween_property(self, "position", position + Vector2.RIGHT * 200, 2)
+		tween.tween_property(self, "position", position + Vector2.LEFT * 200, 2)
 		# vary the pitch
 		$Synth.pitch_scale = randf_range(0.7, 1.3)
 		# wait 2 seconds
